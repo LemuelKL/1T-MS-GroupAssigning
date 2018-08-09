e8c5deab18db8a7bd95480a470013e9c9dd66ec3
LemuelKL <lemuellee.kl@gmail.com>
2018-08-09 23:44:53 +0800

    Settings on Data Structure

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-nTeacher = None
-nStudent = None
+nTeacher = 40
+nStudent = 140
+nCourse = 40
+nSubj = 10
+
+# Example of subj type names
+# chinL
+# engL
+# edu
+# engine
+# math
+# society
+# finance
+# it
+# phy
+# medic
+
+# Example of course idName
+# chinL_1
+# engL_1
+# edu_1
+# edu_2
+# math_1
+# society_1
+# society_2
+# society_3
 
 class teacher:
-    def __init__(self, idName, avCourse):
+    def __init__(self, idName, avSubj):
         self.idName = idName
-        self.nSub = len(avCourse)
-        self.avSubs = avCourse
+        self.nAvSubj = len(avSubj)
+        self.avSubj = avSubj
 
 class course:
-    max_nS = 5
-    def __init__(self, idNo, nAvTeacher, nAvStudent, nID):
+    max_nStudent = 5
+    def __init__(self, subjType, idNo, nAvTeacher, nAvStudent, nID):
         self.idNo = idNo
         self.nAvT = nAvT
         self.nAvS = nAvS
@@ -20,7 +44,14 @@ def main():
 
     mhy = teacher("mhy", ["math1", "phy1"])
     print(mhy.idName)
-    print(mhy.nSub)
-    print(mhy.avSubs)
+    print(mhy.nAvSubj)
+    print(mhy.avSubj)
+
+def constructCourses(subjType, nCourseToConstruct):
+    ret = []
+    for i in range (0, nCourseToConstruct):
+        tempCourse = course(subjType, i+1, None, None, nCourseToConstruct)
+        ret.append()
+    return ret
 
 main()
