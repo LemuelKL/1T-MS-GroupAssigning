5f20e679699a935a17e72644a4f7a07421ce1d7f
LemuelKL <lemuellee.kl@gmail.com>
2018-08-10 20:33:45 +0800

    Added : Generation of Student Data for Testing

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import json
+from pprint import pprint
+from pprint import pformat
+import random
+
 nTeacher = 40
 nStudent = 140
 nCourse = 40
@@ -14,6 +19,7 @@ nSubj = 10
 # it
 # phy
 # medic
+subjMaster = ["chinL", "engL", "edu", "engine", "math", "society", "finance", "it", "phy", "medic"]
 
 # Example of course idName
 # chinL_1
@@ -26,13 +32,24 @@ nSubj = 10
 # society_3
 
 class teacher:
+    avIndex = None
     def __init__(self, idName, avSubj):
         self.idName = idName
         self.nAvSubj = len(avSubj)
         self.avSubj = avSubj
 
+    def calcAvIndex():
+        return
+
+class student:
+    def __init__(self, pyccode, choice1, choice2):
+        self.pyccode = pyccode
+        self.choice1 = choice1
+        self.choice2 = choice2
+
 class course:
     max_nStudent = 5
+    avIndex = None
     def __init__(self, subjType, idNumber, nAvTeacher, nAvStudent, nID):
         self.subjType = subjType
         self.idNumber = idNumber
@@ -41,6 +58,9 @@ class course:
         self.nAvStudent = nAvStudent
         self.nID = nID
 
+    def calcAvIndex():
+        return
+   
 def constructCourses(subjType, nCourseToConstruct):
     ret = []
     for i in range (0, nCourseToConstruct):
@@ -48,9 +68,16 @@ def constructCourses(subjType, nCourseToConstruct):
         ret.append(tempCourse)
     return ret
 
+def genRandomStudents():
+    toRet = []
+    pyccodes = ["%03d" % x for x in range(1, nStudent + 1)]
+    for x in range (0, nStudent):
+        twoPicks = random.sample(subjMaster, 2)
+        s = student(pyccodes[x], twoPicks[0], twoPicks[1])
+        toRet.append(s)
+    return toRet
+    
 def main():
-    print("Hi")
-
     mhy = teacher("mhy", ["math1", "phy1"])
     print(mhy.idName)
     print(mhy.nAvSubj)
@@ -67,4 +94,33 @@ def main():
         print(societyCourse.nID)
         print("==========")
 
-main()
+def objs2Json(objs):
+    outStr = '['
+    for obj in objs:
+        outStr += '{'
+        outStr += '"pyccode":"'
+        outStr += obj.pyccode
+        outStr += '","choice1":"'
+        outStr += obj.choice1
+        outStr += '","choice2":"'
+        outStr += obj.choice2
+        outStr += '"},'
+    outStr = outStr[:-1]
+    outStr += ']'
+    return outStr
+
+def readJson(filename):
+    with open(filename, 'r') as dataIn:
+        return json.load(dataIn)
+
+def writeJson(filename, jsonString):
+    parsed = json.loads(jsonString)
+    with open(filename, 'w') as outfile:
+        json.dump(parsed, outfile, indent = 4, sort_keys = False)
+
+def genRandomStudentData2Json(filename):
+    jsonString = objs2Json(genRandomStudents())
+    writeJson(filename, jsonString)
+        
+genRandomStudentDataToJson('data/studentChoiesTest.json')
+#main()
