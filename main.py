dad2dcd1a7b6a20d60d4c121785822961ca62543
LemuelKL <lemuellee.kl@gmail.com>
2018-08-12 20:59:02 +0800

    Added Teacher Data Generation and related IOs

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ nStudent = 140
 nCourse = 40
 nSubj = 10
 
+# Weighting Constants
+WC_nAvSubj = 10
+WC_nConnection = 7
+
 # Example of subj type names
 # chinL
 # engL
@@ -30,26 +34,38 @@ subjMaster = ["chinL", "engL", "edu", "engine", "math", "society", "finance", "i
 # society_2
 # society_3
 
+listOfStudentObjs = []
+listOfTeacherObjs = []
+listOfCrouseObjs = []
 
 class teacher:
     avIndex = None
+    nConnection = None
     def __init__(self, idName, avSubjs):
         self.idName = idName
         self.nAvSubj = len(avSubjs)
         self.avSubjs = avSubjs
 
     def calcAvIndex():
-        return
+        if not nAvSubj == None or not nConnection == None:
+            avIndex = nAvSubj * WC_nAvSubj + nConnection * WC_nConnection
+            return 'SUCCESS'
 
 class student:
+    avIndex = None
+    nConnection = None
     def __init__(self, pyccode, choice1, choice2):
         self.pyccode = pyccode
         self.choice1 = choice1
         self.choice2 = choice2
 
+    def calcAvIndex():
+        if not nAvSubj == None or not nConnection == None:
+            avIndex = nAvSubj * WC_nAvSubj + nConnection * WC_nConnection
+            return 'SUCCESS'
+
 class course:
     max_nStudent = 5
-    avIndex = None
     def __init__(self, subjType, idNumber, nAvTeacher, nAvStudent, nID):
         self.subjType = subjType
         self.idNumber = idNumber
@@ -57,11 +73,8 @@ class course:
         self.nAvTeacher = nAvTeacher
         self.nAvStudent = nAvStudent
         self.nID = nID
-
-    def calcAvIndex():
-        return
    
-def constructCourses(subjType, nCourseToConstruct):
+def constructNCoursesBySubjType(subjType, nCourseToConstruct):
     ret = []
     for i in range (0, nCourseToConstruct):
         tempCourse = course(subjType, i+1, None, None, nCourseToConstruct)
@@ -76,25 +89,42 @@ def genRandomStudents():
         s = student(pyccodes[x], twoPicks[0], twoPicks[1])
         toRet.append(s)
     return toRet
+
+#TODO	
+def readListOfTeachersFromFile():
+    with open("data/listOfTeachers.txt") as f:
+        content = f.readlines()
+        content = [x.strip() for x in content]
+    return content
+	
+def genRandomAmountOfRandomSubjects():
+    length = random.randint(1, 4)
+    picks = random.sample(subjMaster, length)
+    toRet = []
+    for i in range (0, length):
+    	toRet.append(picks[i])
+    return toRet
+	
+def genRandomTeachers():
+    toRet = []
+    teacherIdNames = readListOfTeachersFromFile()
+    for teacherIdName in teacherIdNames:
+        avSubjs = genRandomAmountOfRandomSubjects()
+        t = teacher(teacherIdName, avSubjs)
+        toRet.append(t)
+    return toRet
+
+def dump(obj):
+  for attr in dir(obj):
+    print("obj.%s = %r" % (attr, getattr(obj, attr)))
     
 def main():
-    mhy = teacher("mhy", ["math1", "phy1"])
-    print(mhy.idName)
-    print(mhy.nAvSubj)
-    print(mhy.avSubjs)
+    listOfStudentObjs = genRandomStudents()
+    listOfTeacherObjs = genRandomTeachers()
+    dump(listOfTeacherObjs[0])
+    writeTeacherData2Json('data/teacherInfo.json', listOfTeacherObjs)
 
-    societyCourses = constructCourses("society", 3)
-    for societyCourse in societyCourses:
-        print("==========")
-        print(societyCourse.idName)
-        print(societyCourse.subjType)
-        print(societyCourse.nAvTeacher)
-        print(societyCourse.nAvStudent)
-        print(societyCourse.idNumber)
-        print(societyCourse.nID)
-        print("==========")
-
-def objs2Json(objs):
+def studentObjs2Json(objs):
     outStr = '['
     for obj in objs:
         outStr += '{'
@@ -109,6 +139,29 @@ def objs2Json(objs):
     outStr += ']'
     return outStr
 
+def teacherObjs2Json(objs):
+    outStr = '['
+    for obj in objs:
+        outStr += '{'
+        outStr += '"idName":"'
+        outStr += obj.idName
+        outStr += '","nAvSubj":"'
+        outStr += str(obj.nAvSubj)
+        outStr += '","avSubjs":"'
+        outStr += flatten2DSubjs(obj.avSubjs)
+        outStr += '"},'
+    outStr = outStr[:-1]
+    outStr += ']'
+    return outStr
+
+def flatten2DSubjs(listOfStrs):
+    result = listOfStrs[0]
+    print(result)
+    for i in range(1, len(listOfStrs)):
+        result += ', '
+        result += listOfStrs[i]   
+    return result
+
 def readJson(filename):
     with open(filename, 'r') as dataIn:
         return json.load(dataIn)
@@ -118,9 +171,12 @@ def writeJson(filename, jsonString):
     with open(filename, 'w') as outfile:
         json.dump(parsed, outfile, indent = 4, sort_keys = False)
 
-def genRandomStudentData2Json(filename):
-    jsonString = objs2Json(genRandomStudents())
+def writeStudentData2Json(filename, objs):
+    jsonString = teacherObjs2Json(objs)
+    writeJson(filename, jsonString)
+
+def writeTeacherData2Json(filename, objs):
+    jsonString = teacherObjs2Json(objs)
     writeJson(filename, jsonString)
         
-genRandomStudentData2Json('data/studentChoies.json')
-#main()
+main()
